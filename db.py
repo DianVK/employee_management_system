a04e66e9c77f56d406fc7b9111876bbcda95bb78
DianVK <diankinanev@abv.bg>
2023-01-06 10:34:20 +0200

    Insert function to db file

--- db.py
+++ db.py
@@ -2,10 +2,10 @@ import sqlite3
 
 
 class Database:
-    def __init__(self,db):
+    def __init__(self, db):
         self.con = sqlite3.connect(db)
         self.cur = self.con.cursor()
-        sql="""
+        sql = """
         CREATE TABLE IF NOT EXISTS employees(
             id Integer Primary Key,
             name text,
@@ -20,5 +20,13 @@ class Database:
         self.cur.execute(sql)
         self.con.commit()
 
+    # Insert Function
+    def insert(self, name, age, doj, email,gender,contact,address):
+        sql = "insert into employees values(NULL,?,?,?,?,?,?,?)"
+        self.cur.execute(sql,(name, age, doj, email,gender,contact,address))
+        self.con.commit()
+
 
 o = Database("Employee.db")
+o.insert("Peter","25","05-10-2022","peter_grigorov@gmail.com","Male","+359892407444","Oborishte 8, Plovdiv")
+o.insert("John","27","08-4-2023","this_is_test_email@gmail.com","Male","+359883457841","Mariya Luiza 103, Sofia")
