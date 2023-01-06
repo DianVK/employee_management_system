b8ef601eab474ca2360a7f6b7305ff950e4cdb5d
DianVK <diankinanev@abv.bg>
2023-01-06 12:15:32 +0200

    Creating the Buttons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
 root.title("Employee Management System")
@@ -17,22 +18,68 @@ contact = StringVar()
 entries_frame = Frame(root, bg="#535c68")
 entries_frame.pack(side=TOP, fill=X)
 title = Label(entries_frame, text="Employee Management System", font=("Calibri", 18, "bold"), bg="#535c68", fg="white")
-title.grid(row=0, columnspan=2, padx=10, pady=20)
+title.grid(row=0, columnspan=2, padx=10, pady=20, sticky="w")
 
 lblName = Label(entries_frame, text="Name", font=("Calibri", 16), bg="#535c68", fg="white")
-lblName.grid(row=1, column=0,padx=10,pady=10)
-txtName = Entry(entries_frame,textvariable=name,font=("Calibri",16),width=30)
-txtName.grid(row=1,column=1,padx=10,pady=10)
+lblName.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+txtName = Entry(entries_frame, textvariable=name, font=("Calibri", 16), width=30)
+txtName.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
 lblAge = Label(entries_frame, text="Age", font=("Calibri", 16), bg="#535c68", fg="white")
-lblAge.grid(row=1, column=2)
-txtAge = Entry(entries_frame,textvariable=age,font=("Calibri",16),width=30)
-txtAge.grid(row=1,column=3)
+lblAge.grid(row=1, column=2, padx=10, pady=10, sticky="w")
+txtAge = Entry(entries_frame, textvariable=age, font=("Calibri", 16), width=30)
+txtAge.grid(row=1, column=3, padx=10, pady=10, sticky="w")
 
-lbldoj = Label(entries_frame, text="D.O.J.", font=("Calibri", 16), bg="#535c68", fg="white")
-lbldoj.grid(row=2, column=0)
-txtDoj = Entry(entries_frame,textvariable=doj,font=("Calibri",16),width=30)
-txtDoj.grid(row=2,column=1)
+lbldoj = Label(entries_frame, text="D.O.J", font=("Calibri", 16), bg="#535c68", fg="white")
+lbldoj.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+txtDoj = Entry(entries_frame, textvariable=doj, font=("Calibri", 16), width=30)
+txtDoj.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+
+lblEmail = Label(entries_frame, text="Email", font=("Calibri", 16), bg="#535c68", fg="white")
+lblEmail.grid(row=2, column=2, padx=10, pady=10, sticky="w")
+txtEmail = Entry(entries_frame, textvariable=email, font=("Calibri", 16), width=30)
+txtEmail.grid(row=2, column=3, padx=10, pady=10, sticky="w")
+
+lblGender = Label(entries_frame, text="Gender", font=("Calibri", 16), bg="#535c68", fg="white")
+lblGender.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+comboGender = ttk.Combobox(entries_frame, font=("Calibri", 16), width=28, textvariable=gender, state="readonly")
+comboGender['values'] = ("Male", "Female")
+comboGender.grid(row=3, column=1, padx=10, sticky="w")
+
+lblContact = Label(entries_frame, text="Contact No", font=("Calibri", 16), bg="#535c68", fg="white")
+lblContact.grid(row=3, column=2, padx=10, pady=10, sticky="w")
+txtContact = Entry(entries_frame, textvariable=contact, font=("Calibri", 16), width=30)
+txtContact.grid(row=3, column=3, padx=10, sticky="w")
+
+lblAddress = Label(entries_frame, text="Address", font=("Calibri", 16), bg="#535c68", fg="white")
+lblAddress.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+
+txtAddress = Text(entries_frame, width=36, height=1, font=("Calibri", 16))
+txtAddress.grid(row=4, column=1, columnspan=4, padx=10, sticky="w")
+
+
+def add_employee():
+    pass
+def update_employee():
+    pass
+def delete_employee():
+    pass
+def clear_all():
+    pass
+
+btn_frame = Frame(entries_frame, bg="#535c68")
+btn_frame.grid(row=6, column=0, columnspan=4, padx=10, pady=10, sticky="w")
+btnAdd = Button(btn_frame, command=add_employee, text="Add Details", width=15, font=("Calibri", 16, "bold"), fg="white",
+                bg="#16a085", bd=0).grid(row=0, column=0)
+btnEdit = Button(btn_frame, command=update_employee, text="Update Details", width=15, font=("Calibri", 16, "bold"),
+                 fg="white", bg="#2980b9",
+                 bd=0).grid(row=0, column=1, padx=10)
+btnDelete = Button(btn_frame, command=delete_employee, text="Delete Details", width=15, font=("Calibri", 16, "bold"),
+                   fg="white", bg="#c0392b",
+                   bd=0).grid(row=0, column=2, padx=10)
+btnClear = Button(btn_frame, command=clear_all, text="Clear Details", width=15, font=("Calibri", 16, "bold"), fg="white",
+                  bg="#f39c12",
+                  bd=0).grid(row=0, column=3, padx=10)
 
 # Table Frame
 root.mainloop()
