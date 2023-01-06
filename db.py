4b1401800903e823d9e59ff1c4b8f57e9125d61c
DianVK <diankinanev@abv.bg>
2023-01-06 15:52:05 +0200

    Update and Remove Employee

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-
 class Database:
     def __init__(self, db):
         self.con = sqlite3.connect(db)
@@ -21,28 +20,26 @@ class Database:
         self.con.commit()
 
     # Insert Function
-    def insert(self, name, age, doj, email,gender,contact,address):
-        sql = "insert into employees values(NULL,?,?,?,?,?,?,?)"
-        self.cur.execute(sql,(name, age, doj, email,gender,contact,address))
+    def insert(self, name, age, doj, email, gender, contact, address):
+        self.cur.execute("insert into employees values (NULL,?,?,?,?,?,?,?)",
+                         (name, age, doj, email, gender, contact, address))
         self.con.commit()
 
-    # Fetch all Data from DB
+    # Fetch All Data from DB
     def fetch(self):
         self.cur.execute("SELECT * from employees")
         rows = self.cur.fetchall()
-        #print(rows)
+        # print(rows)
         return rows
 
     # Delete a Record in DB
-    def remove(self,id):
-        self.cur.execute("delete from employees where id=?",(id,))
+    def remove(self, id):
+        self.cur.execute("delete from employees where id=?", (id,))
         self.con.commit()
 
     # Update a Record in DB
-    def update(self,id,name, age, doj, email,gender,contact,address):
-        self.cur.execute("update employees set name=?,age=?,doj=?,email=?,gender=?,contact=?,address=? where id=?",
-                         (name, age, doj, email,gender,contact,address,id))
+    def update(self, id, name, age, job, email, gender, contact, address):
+        self.cur.execute(
+            "update employees set name=?, age=?, doj=?, email=?, gender=?, contact=?, address=? where id=?",
+            (name, age, job, email, gender, contact, address, id))
         self.con.commit()
-
-
-o = Database("Employee.db")
